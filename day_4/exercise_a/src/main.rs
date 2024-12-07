badf150c4b11c8f0b7d6566e172252136ac7717b
giriel <robin.staes@gmail.com>
2024-12-07 17:03:24 +0100

    First dummy implementation to count the hits

--- day_4/exercise_a/src/main.rs
+++ day_4/exercise_a/src/main.rs
@@ -18,6 +18,23 @@ fn select_file() -> Option<PathBuf> {
         .pick_file();
 }
 
+fn search_xmas(
+    letter: char,
+    input_matrix: &Vec<Vec<char>>,
+    row_index: usize,
+    column_index: usize,
+) -> i32 {
+    return 1;
+}
+fn search_samx(
+    letter: char,
+    input_matrix: &Vec<Vec<char>>,
+    row_index: usize,
+    column_index: usize,
+) -> i32 {
+    return 1;
+}
+
 fn process_file(file_path: PathBuf) {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut xmas_hits = 0;
@@ -34,5 +51,30 @@ fn process_file(file_path: PathBuf) {
     //  vertically in both directions
     //  diagonally in 4 directions
 
-    println!("Number of XMAS hits = {}", xmas_hits);
+    let mut hits_heatmap = vec![vec![0; input_matrix.len()]; input_matrix[0].len()];
+    for row_index in 0..input_matrix.len() {
+        let row = &input_matrix[row_index];
+        for column_index in 0..row.len() {
+            let letter = row[column_index];
+            if letter == 'X' {
+                hits_heatmap[row_index][column_index] =
+                    search_xmas(letter, &input_matrix, row_index, column_index);
+            } else if letter == 'S' {
+                hits_heatmap[row_index][column_index] =
+                    search_samx(letter, &input_matrix, row_index, column_index);
+            }
+        }
+    }
+
+    for row in &hits_heatmap {
+        println!("{:?}", row);
+    }
+
+    println!(
+        "Number of XMAS hits = {}",
+        hits_heatmap
+            .iter()
+            .map(|x| -> i32 { x.iter().sum() })
+            .sum::<i32>()
+    );
 }
