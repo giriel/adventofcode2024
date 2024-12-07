0aa7ec378e6d3bf9a6e74a4eeadc780637c7f7ea
giriel <robin.staes@gmail.com>
2024-12-07 16:39:25 +0100

    Read data into a matrix and write out algorithm

--- day_4/exercise_a/src/main.rs
+++ day_4/exercise_a/src/main.rs
@@ -3,8 +3,6 @@ use std::env;
 use std::fs;
 use std::path::PathBuf;
 
-use regex::Regex;
-
 fn main() {
     println!("Please select a file to process:");
     let file = select_file();
@@ -23,7 +21,18 @@ fn select_file() -> Option<PathBuf> {
 fn process_file(file_path: PathBuf) {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let mut xmas_hits = 0;
-    for report in contents.trim().lines() {}
+
+    let mut input_matrix: Vec<Vec<char>> = Vec::new();
+    for report in contents.trim().lines() {
+        input_matrix.push(report.chars().collect::<Vec<char>>());
+    }
+
+    // X can be the start of XMAS
+    // S can be the start of SAMX
+    // for every of these we should check matches
+    //  horizontally in both directions
+    //  vertically in both directions
+    //  diagonally in 4 directions
 
     println!("Number of XMAS hits = {}", xmas_hits);
 }
