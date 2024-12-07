f08ead309ad5bb4b413eaec1547091a0c37a3f0e
giriel <robin.staes@gmail.com>
2024-12-07 16:14:46 +0100

    day 3 exercise b

---
+++ day_3/exercise_b/src/main.rs
@@ -0,0 +1,68 @@
+#![feature(iter_map_windows)]
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+use regex::Regex;
+
+fn main() {
+    println!("Please select a file to process:");
+    let file = select_file();
+    match file {
+        Some(file) => process_file(file),
+        None => println!("No file selected."),
+    }
+}
+
+fn select_file() -> Option<PathBuf> {
+    return rfd::FileDialog::new()
+        .set_directory(env::current_dir().expect("Cannot fetch current directory?"))
+        .pick_file();
+}
+
+fn process_file(file_path: PathBuf) {
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let mult_regex = Regex::new(
+        r#"(?<do>do\(\))|(?<dont>don\'t\(\))|mul\((?<lhs>[1-9][0-9]*),(?<rhs>[1-9][0-9]*)\)"#,
+    )
+    .unwrap();
+
+    let mut multiplications: Vec<i64> = Vec::new();
+    let mut is_do: bool = true;
+    for report in contents.trim().lines() {
+        let mut multi: Vec<i64> = mult_regex
+            .captures_iter(report)
+            .map(|caps| {
+                if caps.name("do").is_some() {
+                    is_do = true;
+                    return 0;
+                } else if caps.name("dont").is_some() {
+                    is_do = false;
+                    return 0;
+                }
+                if !is_do {
+                    return 0;
+                }
+
+                let lhs: i64 = caps
+                    .name("lhs")
+                    .unwrap()
+                    .as_str()
+                    .parse()
+                    .expect("no number");
+                let rhs: i64 = caps
+                    .name("rhs")
+                    .unwrap()
+                    .as_str()
+                    .parse()
+                    .expect("no number");
+                lhs * rhs
+            })
+            .collect();
+        multiplications.append(&mut multi);
+    }
+
+    let sum: i64 = multiplications.iter().sum();
+    println!("Multiplication result = {}", sum);
+}
